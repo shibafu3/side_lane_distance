428827f7ebafdc56e6b34c88649480d5461ac1ca
shiba <shiba@shiba.com>
2019-10-01 18:25:25 +0900

    日付生成部分をLinux仕様に変更

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,18 @@ int main(char argc, char *argv[]) {
     time_t time_raw = time(nullptr);
 
     //形式を変換する
-    tm date_raw;
-    localtime_s(&date_raw, &time_raw);
+    tm *date_raw;
+    date_raw = localtime(&time_raw);
 
     //sに独自フォーマットになるように連結していく
     std::stringstream s;
     s << "20";
-    s << setfill('0') << setw(2) << date_raw.tm_year - 100; //100を引くことで20xxのxxの部分になる
-    s << setfill('0') << setw(2) << date_raw.tm_mon + 1; //月を0からカウントしているため
-    s << setfill('0') << setw(2) << date_raw.tm_mday; //そのまま
-    s << setfill('0') << setw(2) << date_raw.tm_hour; //そのまま
-    s << setfill('0') << setw(2) << date_raw.tm_min; //そのまま
-    s << setfill('0') << setw(2) << date_raw.tm_sec; //そのまま
+    s << setfill('0') << setw(2) << date_raw->tm_year - 100; //100を引くことで20xxのxxの部分になる
+    s << setfill('0') << setw(2) << date_raw->tm_mon + 1; //月を0からカウントしているため
+    s << setfill('0') << setw(2) << date_raw->tm_mday; //そのまま
+    s << setfill('0') << setw(2) << date_raw->tm_hour; //そのまま
+    s << setfill('0') << setw(2) << date_raw->tm_min; //そのまま
+    s << setfill('0') << setw(2) << date_raw->tm_sec; //そのまま
     std::string video_file_name1 = s.str() + "_1.avi";
     std::string video_file_name2 = s.str() + "_2.avi";
 
